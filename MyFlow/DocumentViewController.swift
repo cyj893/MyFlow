cc8a321912ced20e82ab5b498d65a7c2b2f4d8b0
cyj893 <cyj89317@gmail.com>
2022-05-13 20:55:44 +0900

    Add MyNavigationView

--- MyFlow/DocumentViewController.swift
+++ MyFlow/DocumentViewController.swift
@@ -11,7 +11,7 @@ import SnapKit
 import PDFKit
 
 class DocumentViewController: UIViewController {
-        
+    let myNavigationView = MyNavigationView.singletonView
     var pdfView = PDFView()
     var document: UIDocument?
     
@@ -22,7 +22,6 @@ class DocumentViewController: UIViewController {
         document?.open(completionHandler: { (success) in
             if success {
                 // Display the content of the document, e.g.:
-                print(self.document?.fileURL.lastPathComponent)
                 self.pdfView.document = PDFDocument(url: self.document!.fileURL)
             } else {
                 // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
@@ -32,6 +31,7 @@ class DocumentViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                
         view.addSubview(pdfView)
         pdfView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -41,8 +41,43 @@ class DocumentViewController: UIViewController {
         pdfView.pageBreakMargins = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         pdfView.autoScales = true
         
-        pdfView.backgroundColor = .gray
+        pdfView.backgroundColor = .black
+        
+        
+        view.addSubview(myNavigationView)
+        myNavigationView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(100)
+        }
+        
         
     }
     
+    
+    // MARK: MyNavigationView show and hide
+    
+    func show() {
+        myNavigationView.snp.remakeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(100)
+        }
+        
+        UIView.animate(
+            withDuration: 0.5,
+            animations: self.view.layoutIfNeeded
+        )
+    }
+    
+    func hide() {
+        myNavigationView.snp.remakeConstraints {
+            $0.top.equalToSuperview().offset(-100)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(0)
+        }
+        
+        UIView.animate(
+            withDuration: 0.5,
+            animations: self.view.layoutIfNeeded
+        )
+    }
 }
---
+++ MyFlow/MyNavigationView.swift
@@ -0,0 +1,48 @@
+//
+//  MyNavigationView.swift
+//  MyFlow
+//
+//  Created by Yujin Cha on 2022/05/13.
+//
+
+import UIKit
+import Then
+import SnapKit
+import PDFKit
+
+class MyNavigationView: UIView {
+    static let singletonView = MyNavigationView()
+    
+    let optionsView = UIView()
+    let backButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        self.addSubview(optionsView)
+        optionsView.backgroundColor = .blue
+        optionsView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(100)
+        }
+        
+        self.addSubview(backButton)
+        backButton.then {
+            $0.setTitle("뒤로", for: .normal)
+            $0.backgroundColor = .cyan
+        }.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.top.equalTo(optionsView.snp.top).offset(30)
+        }
+        backButton.addTarget(self, action: #selector(backButtonAction), for: .touchUpInside)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("storyboard only DocumentBrowserViewController")
+    }
+    
+    @objc func backButtonAction() {
+        print("clicked")
+    }
+    
+}
