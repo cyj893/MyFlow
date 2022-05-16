27480912e9714aaf83b8ec36cf4ebda5787d3bdb
cyj893 <cyj89317@gmail.com>
2022-05-17 02:16:56 +0900

    Refactor: Separate methods

--- MyFlow/DocumentViewController.swift
+++ MyFlow/DocumentViewController.swift
@@ -15,9 +15,15 @@ class DocumentViewController: UIViewController, PDFDocumentDelegate {
     var pdfView = PDFView()
     var document: UIDocument?
     var isShowingMyNavigationView: Bool = true
-    var points:[(PDFPage, CGPoint)] = []
+    var points:[(PDFPage, CGFloat)] = []
     
     var idx:Int = 0
+    
+    let gradientColors:[UIColor] = [UIColor(rgb: 0x769FCD, alpha: 0.5),
+                                    UIColor(rgb: 0xB9D7EA, alpha: 0.5),
+                                    UIColor(rgb: 0xD6E6F2, alpha: 0.5),
+                                    UIColor(rgb: 0xF7FBFC, alpha: 0.5)]
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -73,7 +79,7 @@ class DocumentViewController: UIViewController, PDFDocumentDelegate {
         idx %= points.count
         let now = points[idx]
         print(idx)
-        pdfView.go(to: CGRect(origin: now.1, size: CGSize(width: 1, height: -view.frame.height)), on: now.0)
+        pdfView.go(to: CGRect(origin: CGPoint(x: 0, y: now.1), size: CGSize(width: 1, height: -view.frame.height)), on: now.0)
     }
     func nextPointButtonAction() {
         print("nextPointButtonAction")
@@ -81,17 +87,104 @@ class DocumentViewController: UIViewController, PDFDocumentDelegate {
         idx %= points.count
         let now = points[idx]
         print(idx)
-        pdfView.go(to: CGRect(origin: now.1, size: CGSize(width: 1, height: -view.frame.height)), on: now.0)
+        pdfView.go(to: CGRect(origin: CGPoint(x: 0, y: now.1), size: CGSize(width: 1, height: -view.frame.height)), on: now.0)
     }
     
 }
 
-// MARK: Toggle MyNavigationView
+// MARK: Define Tap Gesture
 extension DocumentViewController {
     
     fileprivate func addTapGesture() {
         let taps = UITapGestureRecognizer(target: self, action: #selector(setTapGesture))
         pdfView.addGestureRecognizer(taps)
+    }
+    
+    @objc func setTapGesture(_ recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: pdfView)
+        guard let page = pdfView.page(for: location, nearest: true) else { return }
+        let convertedLocation = pdfView.convert(location, to: page)
+        
+        if myNavigationView.getIsHandlingPoints() {
+            handlePoints(convertedLocation, page)
+            return
+        }
+        if myNavigationView.getIsAddingPoints() {
+            addPoint(convertedLocation.y, page)
+            return
+        }
+        if isShowingMyNavigationView {
+            hideNavigationView()
+            isShowingMyNavigationView = false
+        }
+        else {
+            showNavigationView()
+            isShowingMyNavigationView = true
+        }
+    }
+    
+}
+
+// MARK: Toggle NavigationView
+extension DocumentViewController {
+    
+    fileprivate func showNavigationView() {
+        print("show myNavigationView")
+        myNavigationView.snp.remakeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(100)
+        }
+        pdfView.snp.remakeConstraints {
+            $0.top.equalTo(myNavigationView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        animateIt()
+    }
+    
+    fileprivate func hideNavigationView() {
+        print("hide myNavigationView")
+        myNavigationView.snp.remakeConstraints {
+            $0.top.equalToSuperview().offset(-100)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(0)
+        }
+        pdfView.snp.remakeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        animateIt()
+    }
+    
+    fileprivate func animateIt() {
+        UIView.animate(
+            withDuration: 0.5,
+            animations: self.view.layoutIfNeeded
+        )
+    }
+}
+
+
+// MARK: Handle Points
+
+extension DocumentViewController {
+    
+    fileprivate func handlePoints(_ convertedLocation: CGPoint, _ page: PDFPage) {
+        guard let annotation = page.annotation(at: convertedLocation) else { return }
+        guard let _ = annotation.annotationKeyValues["/isPoint"] else { return }
+        print(annotation)
+    }
+    
+}
+
+
+// MARK: Add the Point
+
+extension DocumentViewController {
+    
+    fileprivate func addPoint(_ height: CGFloat, _ page: PDFPage) {
+        points.append((page, height))
+        addPointLineGradient(height, page)
     }
     
     fileprivate func addPointLine(_ heightPoint: CGPoint, _ page: PDFPage, color: UIColor = .blue) {
@@ -117,91 +210,29 @@ extension DocumentViewController {
         page.addAnnotation(inkAnnotation)
     }
     
-    fileprivate func addPointLineGradient(_ heightPoint: CGPoint, _ page: PDFPage) {
-        let gradientColors:[UIColor] = [UIColor(rgb: 0x769FCD, alpha: 0.5), UIColor(rgb: 0xB9D7EA, alpha: 0.5), UIColor(rgb: 0xD6E6F2, alpha: 0.5), UIColor(rgb: 0xF7FBFC, alpha: 0.5)]
+    fileprivate func addPointLineGradient(_ height: CGFloat, _ page: PDFPage) {
         for i in 0...3 {
-            addPointLine(CGPoint(x: 0.0, y: heightPoint.y - CGFloat(i)), page, color: gradientColors[i])
+            addPointLine(CGPoint(x: 0.0, y: height - CGFloat(i)), page, color: gradientColors[i])
         }
-        addNumber(heightPoint, page)
+        addNumber(height, page)
     }
     
-    fileprivate func addNumber(_ heightPoint: CGPoint, _ page: PDFPage) {
+    fileprivate func addNumber(_ height: CGFloat, _ page: PDFPage) {
         let str = "123"
         let font = UIFont.Fonarto(size: 20)
         let size = str.sizeOfString(font: font!)
-        let pointNumText = PDFAnnotation(bounds: CGRect(origin: CGPoint(x: 10.0, y: heightPoint.y - size.height), size: size), forType: .widget, withProperties: nil)
-        pointNumText.widgetStringValue = "123"
+        let pointNumText = PDFAnnotation(bounds: CGRect(origin: CGPoint(x: 10.0, y: height - size.height), size: size), forType: .widget, withProperties: nil)
+        
+        pointNumText.widgetStringValue = str
         pointNumText.widgetFieldType = .text
+        
         pointNumText.alignment = .center
-        pointNumText.font = UIFont.Fonarto(size: 20)
+        pointNumText.font = font
         pointNumText.fontColor = .systemPink
         pointNumText.color = .clear
         pointNumText.backgroundColor = .clear
+        
         page.addAnnotation(pointNumText)
     }
     
-    @objc func setTapGesture(_ recognizer: UITapGestureRecognizer) {
-        if myNavigationView.getIsHandlingPoints() {
-            let location = recognizer.location(in: pdfView)
-            guard let page = pdfView.page(for: location, nearest: true) else { return }
-            let convertedLocation = pdfView.convert(location, to: page)
-            guard let annotation = page.annotation(at: convertedLocation) else { return }
-            guard let _ = annotation.annotationKeyValues["/isPoint"] else { return }
-            print(annotation)
-            return
-        }
-        if myNavigationView.getIsAddingPoints() {
-            let location = recognizer.location(in: pdfView)
-            guard let page = pdfView.page(for: location, nearest: true) else { return }
-            let heightPoint = CGPoint(x: 0, y: pdfView.convert(location, to: page).y)
-            print(heightPoint)
-            points.append((page, heightPoint))
-            
-            addPointLineGradient(heightPoint, page)
-            return
-        }
-        if isShowingMyNavigationView {
-            hide()
-            isShowingMyNavigationView = false
-        }
-        else {
-            show()
-            isShowingMyNavigationView = true
-        }
-    }
-    
-    fileprivate func show() {
-        print("show myNavigationView")
-        myNavigationView.snp.remakeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
-            $0.height.equalTo(100)
-        }
-        pdfView.snp.remakeConstraints {
-            $0.top.equalTo(myNavigationView.snp.bottom)
-            $0.leading.trailing.bottom.equalToSuperview()
-        }
-        
-        animateIt()
-    }
-    
-    fileprivate func hide() {
-        print("hide myNavigationView")
-        myNavigationView.snp.remakeConstraints {
-            $0.top.equalToSuperview().offset(-100)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(0)
-        }
-        pdfView.snp.remakeConstraints {
-            $0.edges.equalToSuperview()
-        }
-        
-        animateIt()
-    }
-    
-    fileprivate func animateIt() {
-        UIView.animate(
-            withDuration: 0.5,
-            animations: self.view.layoutIfNeeded
-        )
-    }
 }
