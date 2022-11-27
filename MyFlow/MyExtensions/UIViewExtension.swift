9d9be4da40e666c893d5cab3a5187532e2006d7b
cyj893 <cyj89317@gmail.com>
2022-11-27 18:19:01 +0900

    feat: Add `addInnerShadow()`

--- MyFlow/MyExtensions/UIViewExtension.swift
+++ MyFlow/MyExtensions/UIViewExtension.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 extension UIView {
-    func addShadow() {
+    func addShadow(opacity: Float = 0.5, radius: CGFloat = 5.0) {
         layer.masksToBounds = false
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowOffset = CGSize(width: 0, height: 0)
-        layer.shadowOpacity = 0.5
-        layer.shadowRadius = 5.0
-        layer.shadowPath = nil
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+    }
+    
+    func addInnerShadow(opacity: Float = 0.5, radius: CGFloat = 1.0) {
+        if layer.sublayers?.contains(where: { $0.name == "innerShadow" }) ?? true {
+            return
+        }
+        
+        let innerShadow = CALayer()
+        innerShadow.name = "innerShadow"
+        innerShadow.frame = bounds
+        
+        let shadowPath = UIBezierPath(roundedRect: innerShadow.bounds.insetBy(dx: radius, dy: radius), cornerRadius: layer.cornerRadius)
+        let cutout = UIBezierPath(roundedRect: innerShadow.bounds, cornerRadius: layer.cornerRadius).reversing()
+        
+        shadowPath.append(cutout)
+        innerShadow.shadowPath = shadowPath.cgPath
+        
+        innerShadow.masksToBounds = true
+        
+        innerShadow.shadowColor = UIColor.black.cgColor
+        innerShadow.shadowOffset = CGSize(width: 0, height: 0)
+        innerShadow.shadowOpacity = opacity
+        innerShadow.shadowRadius = radius
+        innerShadow.cornerRadius = layer.cornerRadius
+        layer.addSublayer(innerShadow)
     }
 }
