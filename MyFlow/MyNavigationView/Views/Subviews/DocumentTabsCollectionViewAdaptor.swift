71618d63ecdb5f8e1e0680456939c4a500908da8
cyj893 <cyj89317@gmail.com>
2022-11-27 20:32:43 +0900

    feat: Add `DocumentTabsCollectionViewAdaptor`
    Sets tabs collectionView.
    Can drag&drop a tab.

---
+++ MyFlow/MyNavigationView/Views/Subviews/DocumentTabsCollectionViewAdaptor.swift
@@ -0,0 +1,118 @@
+//
+//  DocumentTabsCollectionViewAdaptor.swift
+//  MyFlow
+//
+//  Created by Yujin Cha on 2022/11/27.
+//
+
+import UIKit
+
+
+final class DocumentTabsCollectionViewAdaptor: NSObject {
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 300, height: 30)
+        layout.minimumLineSpacing = 10
+        
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsHorizontalScrollIndicator = false
+        
+        return cv
+    }()
+    
+    var tabs: [String] = ["asd", "asd2", "asd3", "asd4", "asd5", "asd6"]
+    
+    override init() {
+        super.init()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.dragInteractionEnabled = true
+        collectionView.dragDelegate = self
+        collectionView.dropDelegate = self
+        
+        collectionView.register(TabCell.self, forCellWithReuseIdentifier: "TabCell")
+    }
+}
+
+
+extension DocumentTabsCollectionViewAdaptor: UICollectionViewDelegate {
+    
+}
+
+
+extension DocumentTabsCollectionViewAdaptor: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tabs.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TabCell", for: indexPath) as! TabCell
+        let item = tabs[indexPath.row]
+        cell.label.text = item
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+}
+
+
+extension DocumentTabsCollectionViewAdaptor: UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        return [UIDragItem(itemProvider: NSItemProvider())]
+    }
+}
+
+
+extension DocumentTabsCollectionViewAdaptor: UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        if session.localDragSession != nil {
+            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        }
+        return UICollectionViewDropProposal(operation: .cancel, intent: .unspecified)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        var destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: tabs.count - 1, section: 0)
+        
+        if coordinator.proposal.operation == .move {
+            if let item = coordinator.items.first, let sourceIndexPath = item.sourceIndexPath {
+                collectionView.performBatchUpdates {
+                    let temp = self.tabs[sourceIndexPath.item]
+                    self.tabs.remove(at: sourceIndexPath.item)
+                    self.tabs.insert(temp, at: destinationIndexPath.item)
+                    
+                    collectionView.deleteItems(at: [sourceIndexPath])
+                    collectionView.insertItems(at: [destinationIndexPath])
+                }
+                coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+            }
+        }
+    }
+}
+
+
+
+final class TabCell: UICollectionViewCell {
+    lazy var label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        backgroundColor = .systemMint
+        
+        contentView.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
