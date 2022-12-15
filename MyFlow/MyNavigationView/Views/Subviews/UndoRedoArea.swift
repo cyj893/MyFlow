11771ac4997c6f4eabfa5c919c340c60c8502f7f
cyj893 <cyj89317@gmail.com>
2022-12-16 00:22:44 +0900

    refactor: Add `UIControlExtension`

--- MyFlow/MyNavigationView/Views/Subviews/UndoRedoArea.swift
+++ MyFlow/MyNavigationView/Views/Subviews/UndoRedoArea.swift
@@ -67,10 +67,3 @@ extension UndoRedoArea {
         }
     }
 }
-
-
-extension UIControl {
-    func addAction(for controlEvents: UIControl.Event = .touchUpInside, _ closure: @escaping ()->()) {
-        addAction(UIAction { (action: UIAction) in closure() }, for: controlEvents)
-    }
-}
