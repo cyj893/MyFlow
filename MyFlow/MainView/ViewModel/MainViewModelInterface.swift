0e164d283e12c0737c55b97ec1b8a25720bfe336
cyj893 <cyj89317@gmail.com>
2022-12-15 20:48:11 +0900

    refactor: Add `MainViewModelInterface`

---
+++ MyFlow/MainView/ViewModel/MainViewModelInterface.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewModelInterface.swift
+//  MyFlow
+//
+//  Created by Yujin Cha on 2022/12/15.
+//
+
+
+protocol MainViewModelInterface {
+    func openDocument(_ vc: DocumentViewController)
+    func getNowDocumentViewController() -> DocumentViewController
+    func changeCurrentDocumentState(to state: DocumentViewState)
+}
