2e1dd7e6d13b4208a14b48a279dcafb5d29b91e3
cyj893 <cyj89317@gmail.com>
2023-02-23 19:32:59 +0900

    feat: Add Setting for using TrueDepth
    An alert is displayed according to the permission status of the user.

--- MyFlow/Views/SettingsView/Subviews/PlayModeSettingView/PlayModeSettingView.swift
+++ MyFlow/Views/SettingsView/Subviews/PlayModeSettingView/PlayModeSettingView.swift
@@ -9,6 +9,8 @@ import UIKit
 import Then
 import SnapKit
 
+import AVFoundation
+
 
 protocol PlayModeSettingDelegate: NSObject {
     func present(_ vc: UIViewController)
@@ -19,6 +21,8 @@ extension PlayModeSettingView {
     static let title = "In PlayMode"
     static let autoScaleTitle = "Auto Scale When Start"
     static let tapAreaTitle = "Set Tap Area"
+    static let trueDepthTitle = "Move to Next Point with Head Bowing"
+    static let trueDepthBody = "You can move to the next point by lowering your head(get closer to the camera) and raising your head(move away from camera).\nA TrueDepth camera must be available."
     
 }
 
@@ -38,6 +42,9 @@ final class PlayModeSettingView: UIView, ExpandableSettingViewStyle {
                                           type: .toggleable(UserDefaults.playModeAutoScale))
     var tapAreaLabel = SettingStackCell(title: PlayModeSettingView.tapAreaTitle,
                                         type: .withIcon("chevron.right"))
+    var trueDepthLabel = SettingStackCell(title: PlayModeSettingView.trueDepthTitle,
+                                          body: PlayModeSettingView.trueDepthBody,
+                                          type: .toggleable(UserDefaults.useTrueDepth))
     
     
     init() {
@@ -61,7 +68,7 @@ final class PlayModeSettingView: UIView, ExpandableSettingViewStyle {
 extension PlayModeSettingView {
     private func addSubviews() {
         content.addSubview(stackView)
-        [autoScaleLabel, tapAreaLabel].forEach { subview in
+        [autoScaleLabel, tapAreaLabel, trueDepthLabel].forEach { subview in
             stackView.addArrangedSubviewWithDivider(subview)
         }
         stackView.arrangedSubviews.forEach { subview in
@@ -84,7 +91,30 @@ extension PlayModeSettingView {
             vc.modalPresentationStyle = .fullScreen
             self.delegate?.present(vc)
         }
-        
+        trueDepthLabel.addConditionalToggleAction { [unowned self] nowState in
+            if nowState { // The user does not want to use
+                UserDefaults.useTrueDepth = false
+                return false
+            }
+            switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized: // The user has previously granted access to the camera
+                UserDefaults.useTrueDepth = true
+                return true
+            case .notDetermined: // The user has not yet been presented with the option to grant access
+                AVCaptureDevice.requestAccess(for: .video, completionHandler: { [weak self] granted in
+                    if granted {
+                        UserDefaults.useTrueDepth = granted
+                        DispatchQueue.main.async { [weak self] in
+                            self?.trueDepthLabel.updateToggleState(with: granted)
+                        }
+                    }
+                })
+                return false
+            default: // The user has previously denied access
+                delegate?.present(AlertMaker.trueDepthAlert())
+                return false
+            }
+        }
 #if DEBUG
         titleLabel.backgroundColor = .systemCyan
         content.backgroundColor = .systemPink.withAlphaComponent(0.3)
